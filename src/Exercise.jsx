892e82ada28b82fdb9f0a9e373db0944014f57a0
HaritsMartsyabel <abeljepara5454@gmail.com>
2025-11-26 01:51:23 +0700

    update uts

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from "react";
 import UserCard from "./UserCard";
-import { getUsers } from "./Services";
+import { getUsers, getPosts } from "./Services";
+import PostCard from "./PostCard";
 
 function Exercise() {
   const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await getUsers();
-        setUsers(data);
+        const dataUsers = await getUsers();
+        setUsers(dataUsers);
+        setPosts(getPosts);
+
       } catch (error) {
-        console.error("[Component] Gagal menampilkan data:", error.message);
+        console.error("[Component] Error:", error.message);
       }
     };
+    
     fetchData();
   }, []);
 
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 py-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
-        </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          {users.map((user) => (
-            <UserCard key={user.id} {...user} />
-          ))}
-        </div>
+    <div className="min-h-screen bg-gray-100 py-6 px-48">
+      <h1 className="text-3xl font-bold text-center mb-6 text-special-red2">
+        Post Cards
+      </h1>
+      
+      <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-6 max-w-full mx-auto px-4">
+        
+        {posts.map((post) => (
+          <PostCard 
+            key={post.id} 
+            title={post.title} 
+            body={post.body} 
+          />
+        ))}
+
       </div>
-    </>
+    </div>
   );
 }
 
